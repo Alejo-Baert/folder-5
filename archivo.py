4b21f6854428e192c80550248a66fed9ad930980
Brisa <brschirino@gmail.com>
2026-03-02 20:40:08 -0300

    test

---
+++ archivo.py
@@ -0,0 +1,9 @@
+# print("Hola")
+
+for n in range(1, 11):
+    d= 0
+    for c in range(1, n + 1):
+        if n % c == 0:
+            d+=1   
+    if d <= 2:
+        print(n, end=" ")                
